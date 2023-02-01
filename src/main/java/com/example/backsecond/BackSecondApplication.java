491b666c83af7948b4718c5ffcb15ef0abddedf1
SInjuju <qhrrl3891@gmail.com>
2023-02-01 23:31:29 +0900

    .

---
+++ src/main/java/com/example/backsecond/BackSecondApplication.java
@@ -0,0 +1,13 @@
+package com.example.backsecond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackSecondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackSecondApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backsecond/BackSecondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backsecond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackSecondApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
